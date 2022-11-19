093209233978992314cd7668c3ffeede24c16e9e
Lindsor <marcelopluz@gmail.com>
2022-11-19 16:42:09 -0500

    Initial Commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  printWidth: 80,
+};
